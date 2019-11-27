79e3af63eb5cdbfaf1c9c0ee41717224fe22dd0b
Saghelyi, Peter <psaghelyi@gmail.com>
2019-11-27 15:04:14 +0100

    hello world

---
+++ obj2stl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  obj2stl
+//
+//  Created by Peter Saghelyi on 2019. 11. 27..
+//  Copyright © 2019. psaghelyi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
